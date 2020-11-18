18ca95901da136314d367f5043b82bd228c26550
Jonas Hermon-Duc <jonas.hermonduc@gmail.com>
2020-11-18 14:00:50 +0100

    models avec validation

--- db/migrate/20201118112534_create_reviews.rb
+++ db/migrate/20201118112534_create_reviews.rb
@@ -2,8 +2,8 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.integer :rating
-      t.string :content
-      t.string :restaurant
+      t.text :content
+      t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
     end
